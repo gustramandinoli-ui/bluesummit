0310be52c2438ebe20eba0d5bda6531e97b9e4f1
Gustavo <gustramandinoli@hotmail.com>
2026-04-01 16:12:23 -0300

    Adiciona Google Ads gtag (AW-18046482128) e conversões Contato/Inscrição em todas as páginas

--- script.js
+++ script.js
@@ -116,6 +116,17 @@ contactForm.addEventListener('submit', (e) => {
 
   if (valid) {
     const formData = new FormData(contactForm);
+
+    // Dispara conversões do Google Ads diretamente
+    if (typeof gtag === 'function') {
+      gtag('event', 'conversion', {
+        'send_to': 'AW-18046482128/oPZDCOat-5McENDtnZ1D'
+      });
+      gtag('event', 'conversion', {
+        'send_to': 'AW-18046482128/zcljCMroh5EcENDtnZ1D'
+      });
+    }
+
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
